281a6ebd51e219027c071e9984945bb85c7c040e
Srikant <85601026+Srikant67@users.noreply.github.com>
2023-11-20 14:56:28 +0530

    Update AdvertWidget.jsx

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -7,7 +7,7 @@ const AdvertWidget = () => {
   const dark = palette.neutral.dark;
   const main = palette.neutral.main;
   const medium = palette.neutral.medium;
-
+const advertImg = `${URL}/assets/info4.jpeg`
   return (
     <WidgetWrapper>
       <FlexBetween>
@@ -20,7 +20,7 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src=`${URL}/assets/info4.jpeg`
+        src={advertImg}
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
